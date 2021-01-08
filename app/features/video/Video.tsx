3cb15471470ab71759fdf3f4079d270bfc0704cf
Daniel Luan <dluan@uwaterloo.ca>
2021-01-08 13:59:47 -0500

    Fix video list rerender bug (#41)
    * Fix video list rerender bug
    
    * remove dev console logging

--- app/features/video/Video.tsx
+++ app/features/video/Video.tsx
@@ -191,8 +191,7 @@ function DrawerList(props: CollapsibleProps): JSX.Element {
       data: CourseData
     ): void {
       if (type === FileListUpdateType.Video) {
-        console.log('handleCourseDataChanges called');
-        setFiles(data.video);
+        setFiles([...data.video]);
       }
     }
     FileList.subscribeToCourseDataChanges(course, handleCourseDataChanges);
